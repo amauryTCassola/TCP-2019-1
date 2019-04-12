d7eb20a7c09e57d245d193309a72e3318600af2f
Amaury <amaury_cassola@yahoo.com.br>
2019-04-12 00:12:24 -0300

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Amaury
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Death to capitalism");
+
+	}
+
+}
