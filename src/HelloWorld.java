bca5f410417a02510d8df28a815c6dbb677993c7
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2019-05-02 23:08:27 -0300

    Imported and configured JUnit 5 into the project

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,9 @@
  */
 
 import org.jfugue.player.Player;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Amaury, Felipe and Rafael
@@ -17,5 +20,19 @@ public class HelloWorld {
 		System.out.print("Java fucking sucks ass");
 		Player player = new Player();
 		player.play("A B C D E F G");
+	}
+
+	@Test
+	@DisplayName("Exemplo de Teste")
+	public void helloTest() {
+		int exemplo = 5;
+		assertEquals(exemplo, 5);
+	}
+
+	@Test
+	@DisplayName("Another Test")
+	public void helloTest2() {
+		String testString = "Teste";
+		assertEquals(testString, "Teste");
 	}
 }
